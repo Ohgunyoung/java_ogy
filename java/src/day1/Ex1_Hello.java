eb2fc4fa1cba9f1b8deffe51651581c40ba5685c
Ohgunyoung <104331525+Ohgunyoung@users.noreply.github.com>
2022-04-25 12:48:18 +0900

    Ex1_Hello

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Ex1_Hello {
+	// 콘솔 응용 프로그램이 실행되기 위해서 main아리는 메소드가 필요하다)
+	public static void main(String[] args) {
+		/*여러줄 주석
+		 * 주석1
+		 * 주석2
+		 * */
+		/* println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈을 함
+		 * print(문자열) ㅣ 문자열을 콘솔에 출력
+		 */
+		System.out.print("Hello world!!");
+		System.out.println("Hello world!!"); // 한줄 주석은, 주석기호가 나온 시점부터 해당 라인 끝까지 주석처리
+		System.out.print("Hello world!!");
+	}
+
+}
