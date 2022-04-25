a4f2adf5ac6fb5e1ccce511f112da9025209537d
Ohgunyoung <104331525+Ohgunyoung@users.noreply.github.com>
2022-04-25 16:51:58 +0900

    자바 1 일차 : 연습 문제

---
+++ java/src/day1/EX_Test.java
@@ -0,0 +1,36 @@
+package day1;
+
+public class EX_Test {
+
+	public static void main(String[] args) {
+		/*1. 과일가게에서 과일 판매량을 기록하려 한다.
+		 * 사과, 오렌지 판매량을 기록하기 위해 초기화 변수 값을 선언하세요 
+		 */
+
+	int apple = 0;
+	int oragne = 0;
+	
+		/* 2-1 학생 한명의 국어, 영어, 수학 성적을 저장하려 한다
+		 * 이 때 필요한 초기화 변수 선언을 하세요
+		 * 성적은 정수로 관리 됩니다.
+		 */
+	
+	byte 국어 = 0;
+	byte 영어 = 0;
+	byte 수학 = 0;
+	
+	float 국어2 = 0f;
+	float 영어2 = 0f;
+	float 수학2 = 0f;
+	
+	System.out.println(국어2);
+	
+		/* 2-2. 학생의 학점울 출력하려고 한다.
+		 * 		이 때 학생의 학점을 저장한는 변수를 선언하세요
+		 */
+	float score1 = 0.0f;
+	char scroe2 = 'F';
+	
+	}
+
+}
