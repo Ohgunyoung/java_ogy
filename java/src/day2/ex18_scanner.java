edc3ac4c6c211477612c71c8e03c289cb1fd3309
Ohgunyoung <104331525+Ohgunyoung@users.noreply.github.com>
2022-04-26 17:12:20 +0900

    Update ex18_scanner.java

--- java/src/day2/ex18_scanner.java
+++ java/src/day2/ex18_scanner.java
@@ -1,10 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class ex18_scanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/* 콘솔창에서 정수를 입력하는 예제
+		 * system.in : 콘솔에서 입력
+		 */
+		Scanner scan = new Scanner(system.in);
+		System.out.println("정수를 입력하세요");
+		int num1 = scan.nextInt();
+		System.out.println("입력한 정수는 "num" 입니다" );
+		scan.close();
 	}
 
 }
