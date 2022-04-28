96707e1902d18e8c190ece9db1146d629f77ece8
Ohgunyoung <104331525+Ohgunyoung@users.noreply.github.com>
2022-04-28 17:19:16 +0900

    5day

---
+++ java/src/day5/ReverseNumber.java
@@ -0,0 +1,20 @@
+package day5;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		/* 양의 정수를 입력받아 입력받은 정수를 역으로 출력하는 코드를 작성하세요
+		 * 1230 => 0을 출력을 하고 1230 => 123으로 만듬
+		 * 123 => 3을 출력을 하고 123 => 12
+		 * 12에 => 2를 출력을 하고 12=>1
+		 * 1 => 1을 출력, 1 => 0
+		 * 
+		 * 반복 횟수 : num가 0이 아닐때까지
+		 * 규칙성 : num의 일의 자리를 출력 한 후 num를 10으로 나눈 목을 num에 다시 저장
+		 */
+		Scanner scan = new Scanner(System.in);
+	
+	}
+}
